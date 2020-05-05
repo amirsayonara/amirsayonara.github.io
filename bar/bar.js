ea7ff49257cb192d8df93d4272964b02ed48ce3a
Amir Sayonara <amirsayonara@gmail.com>
2020-05-05 23:14:23 +0700

    Penerapan cookies saat mengatur tema halaman

--- bar/bar.js
+++ bar/bar.js
@@ -1,5 +1,4 @@
 ( function( $ ) {
-
 	$('.theme_panel .toggle_bts').on('click', 'a.sett', function(){
 		if($(this).hasClass('active')) {
 			$(this).removeClass('active');
@@ -20,12 +19,15 @@
 		var color = $(this).attr('data-color');
         
         if( !$('#demo_animate_css').length ) {
+			setCookie('warna', color, 365*3);
         	$('head').append('<link rel="stylesheet" id="demo_layout_css" href="css/template-colors/'+color+'.css" />');
         } else {
         	$('#demo_layout_css').attr('href', 'css/template-colors/'+color+'.css');
         }
         return false;
 	});
+
+	if (getCookie('warna')!='') $('head').append('<link rel="stylesheet" id="demo_layout_css" href="css/template-colors/'+getCookie('warna')+'.css" />');
     
     //dark panels
     $('.theme_panel .dark_style').on('click', 'a', function(){
@@ -35,13 +37,18 @@
 		var dark = $(this).attr('data-dark');
 
 		if(dark == 'dark') {
+			setCookie('tema', 'gelap', 365*3);
 			if (!$('#demo_dark_css').length)
-            	$('head').append('<link rel="stylesheet" id="demo_dark_css" href="css/template-dark/dark.css" />');
+				$('head').append('<link rel="stylesheet" id="demo_dark_css" href="css/template-dark/dark.css" />');
+			document.cookie
         } else if (dark == 'light') {
+			setCookie('tema', 'terang', 365*3);
             $('#demo_dark_css').remove();
         }
 
         return false;
 	});
+
+	if (getCookie('tema')=='gelap') $('head').append('<link rel="stylesheet" id="demo_dark_css" href="css/template-dark/dark.css" />');
 
 } )( jQuery );
